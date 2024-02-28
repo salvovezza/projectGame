4a1bd8bf2f7f7f9e628212db64136a4fca37a160
salvovezza <155612782+salvovezza@users.noreply.github.com>
2024-02-28 18:15:49 +0000

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,12 +1,18 @@
-if (score === null) {
-  score = {
-    wins: 0,
-    losses: 0,
-    ties: 0
-  };
-} 
-  /*
-  when we updated our score in this part of the code: 
+let score = JSON.parse(localStorage.getItem('score'));
+    
+     /*The 'score' will be 'null' because we removed it earlier, and now this 'if' statement will give it a default score.*/
+    if (score === null) {
+      score = {
+        wins: 0,
+        losses: 0,
+        ties: 0
+      };
+    } 
+   
+    updateScoreElement(); 
+
+
+    /*when we updated our score in this part of the code: 
     if (result === 'You win.') {
       score.wins += 1;
     } else if (result === 'You lose.') {
@@ -20,77 +26,75 @@ if (score === null) {
     let score = JSON.parse(localStorage.getItem('score'));
   up here we're going to load the score that we jsut saved from localStorage using 'localStorage.getItem', and the score was saved as a JSON string, so we need to convert it back into an object using 'JSON.parse'. So, that's how we use the JSON object and the localStorage object to save our score more permanently. 
   And the last feature we're going to add is when we click the 'Reset Score' button, we also need to remove the score from localStorage; otherwise, if we press Reset Score, and refresh and then play the game, it will just grab the old score from localStorage;
-  
-  */
+    */  
 
-updateScoreElement();  
+    function playGame(playerMove) {
+        const computerMove = pickComputerMove();
 
-function playGame(playerMove) {
-  const computerMove = pickComputerMove();
+        let result = '';    
 
-  let result = '';    
+        if (playerMove === 'scissors') {
+          if (computerMove === 'scissors') {
+            result = 'Tie';
+          } else if (computerMove === 'rock') {
+            result = 'You lose';
+          } else if (computerMove === 'paper') {
+            result = 'You win';
+          }    
 
-  if (playerMove === 'scissors') {
-    if (computerMove === 'scissors') {
-      result = 'Tie';
-    } else if (computerMove === 'rock') {
-      result = 'You lose';
-    } else if (computerMove === 'paper') {
-      result = 'You win';
-    }    
+        } else if (playerMove === 'paper') {
+            if (computerMove === 'paper') {
+              result = 'Tie';
+            } else if (computerMove === 'scissors') {
+              result = 'You lose';
+            } else if (computerMove === 'rock') {
+              result = 'You win';
+            }    
+            
+        } else if (playerMove === 'rock') {
+          if (computerMove === 'rock') {
+            result = 'Tie';
+          } else if (computerMove === 'paper') {
+            result = 'You lose';
+          } else if (computerMove === 'scissors') {
+            result = 'You win';
+          } 
+        }        
 
-  } else if (playerMove === 'paper') {
-      if (computerMove === 'paper') {
-        result = 'Tie';
-      } else if (computerMove === 'scissors') {
-        result = 'You lose';
-      } else if (computerMove === 'rock') {
-        result = 'You win';
-      }    
-      
-  } else if (playerMove === 'rock') {
-    if (computerMove === 'rock') {
-      result = 'Tie';
-    } else if (computerMove === 'paper') {
-      result = 'You lose';
-    } else if (computerMove === 'scissors') {
-      result = 'You win';
-    } 
-  }        
+        if (result === 'You win') {          
+          score.wins += 1;
+        } 
+        else if (result === 'You lose') {
+          score.losses += 1;
+        } else if (result === 'Tie') {
+          score.ties += 1;
+        }
 
-  if (result === 'You win') {          
-    score.wins += 1;
-  } 
-  else if (result === 'You lose') {
-    score.losses += 1;
-  } else if (result === 'Tie') {
-    score.ties += 1;
-  }
+        localStorage.setItem('score', JSON.stringify(score)); /*using localStorage we are able to get the value that we saved earlier, even though we refresh the page; so, we save our score into localStorage so it doesn't get reset when we refresh the page.
+        */
 
-  localStorage.setItem('score', JSON.stringify(score)); 
+        updateScoreElement();
 
-  updateScoreElement();
+        document.querySelector('.js-result').innerHTML = result;
 
-  document.querySelector('.js-result').innerHTML = result;
+        document.querySelector('.js-moves').innerHTML = `You chose: <img src="${playerMove}-emoji.png" class="smaller-moveicon"> &nbsp; - &nbsp;&nbsp;Computer got: <img src="${computerMove}-emoji.png" class="smaller-moveicon">` ;      
+        }
 
-  document.querySelector('.js-moves').innerHTML = `You chose: <img src="${playerMove}-emoji.png" class="smaller-moveicon"> &nbsp; - &nbsp;&nbsp;Computer got: <img src="${computerMove}-emoji.png" class="smaller-moveicon">` ;      
-  }
+        function updateScoreElement() {
+        document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+        }
 
-  function updateScoreElement() {
-  document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
-  }
+        function pickComputerMove() {
+        const randomNumber = Math.random(); 
 
-  function pickComputerMove() {
-  const randomNumber = Math.random(); 
+        let computerMove = '';
 
-  let computerMove = '';
-
-  if (randomNumber >= 0 && randomNumber < 1/3) {        
-    computerMove = 'rock';
-  } else if (randomNumber >= 1 / 3 && randomNumber < 2/3) {        
-    computerMove = 'paper';
-  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {       
-    computerMove = 'scissors'; 
-  }
-  return computerMove; 
-}       
+        if (randomNumber >= 0 && randomNumber < 1/3) {        
+          computerMove = 'rock';
+        } else if (randomNumber >= 1 / 3 && randomNumber < 2/3) {        
+          computerMove = 'paper';
+        } else if (randomNumber >= 2 / 3 && randomNumber < 1) {       
+          computerMove = 'scissors'; 
+        }
+        return computerMove; 
+      }       
