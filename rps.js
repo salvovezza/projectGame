9d304fbadd3ecbf8f9ba025eb51011afecd837fc
salvovezza <155612782+salvovezza@users.noreply.github.com>
2024-02-28 12:00:03 +0000

    Update rps.js

--- rps.js
+++ rps.js
@@ -5,6 +5,23 @@ if (score === null) {
     ties: 0
   };
 } 
+  /*
+  when we updated our score in this part of the code: 
+    if (result === 'You win.') {
+      score.wins += 1;
+    } else if (result === 'You lose.') {
+      score.losses += 1
+    } else if (result === 'Tie.') {
+      score ties += 1;
+    }
+  After we update the score, we're going to save it in local storage using local storage dot setItem. Now, local storage only supports strings, so we have to convert our score object into a JSON string using JSON dot 'stringify':
+  localStorage.setItem('score', JSON.stringify(score));
+  And then, after we've saved this into localStorage, when we load the page, up here: 
+    let score = JSON.parse(localStorage.getItem('score'));
+  up here we're going to load the score that we jsut saved from localStorage using 'localStorage.getItem', and the score was saved as a JSON string, so we need to convert it back into an object using 'JSON.parse'. So, that's how we use the JSON object and the localStorage object to save our score more permanently. 
+  And the last feature we're going to add is when we click the 'Reset Score' button, we also need to remove the score from localStorage; otherwise, if we press Reset Score, and refresh and then play the game, it will just grab the old score from localStorage;
+  
+  */
 
 updateScoreElement();  
 
